f213708cbafaa64fd8641890baab8e2f1b57fd0c
gtagamer127721 <tommydockree1@gmail.com>
2022-05-20 12:19:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = Jojosp1[Math.floor(Math.random() * Jojosp1.length)]
+let rightGuessString = Jojos[Math.floor(Math.random() * Jojos.length)]
 
 console.log(Date())
 console.log(rightGuessString)
